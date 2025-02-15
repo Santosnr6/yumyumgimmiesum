6a847a34be689b1952a8fd3c1a03a5ba4edd5fe3
Jesper Nyberg <91jesnyb@gafe.molndal.se>
2025-02-15 15:50:23 +0100

    layout finished

--- packages/base/icon/ui/index.jsx
+++ packages/base/icon/ui/index.jsx
@@ -1,9 +1,13 @@
 import "./index.css";
+import { Link } from "react-router-dom";
+
 
 export const Icon = () => {
     return (
-        <figure className="icon">
-            <img src="./src/assets/yygs.png" alt="Page Logotype" className="icon__image" />
-        </figure>
+        <Link to="/menu" className="link">
+            <figure className="icon">
+                <img src="./src/assets/yygs.png" alt="Page Logotype" className="icon__image" />
+            </figure>
+        </Link>
     );
 }
--- packages/base/menuList/ui/index.jsx
+++ packages/base/menuList/ui/index.jsx
@@ -2,23 +2,28 @@ import "./index.css";
 import { toUpper } from '@yumyumApp/utils';
 import { useGetMenuQuery } from '@yumyumApp/api';
 import { MenuListItem } from "@yumyumApp/menuListItem";
+import { MenuSpecialListItem } from "@yumyumApp/menuSpecialListItem";
 import { useEffect, useState } from "react";
 
 export const MenuList = () => {
     const [food, setFood] = useState([]);
     const [dips, setDips] = useState([]);
+    const [drinks, setDrinks] = useState([]);
     const { data, isError, isLoading } = useGetMenuQuery();
     
     useEffect(() => {
         if(!isLoading) {
             const foodItems = data.items.filter(i => i.type === 'wonton');
             const dipItems = data.items.filter(i => i.type === 'dip');
+            const drinkItems = data.items.filter(i => i.type === 'drink');
             setFood(foodItems);
             setDips(dipItems);
+            setDrinks(drinkItems);
         }        
     }, [data]);
 
     useEffect(() =>{
+        console.log(data?.items);
         console.log(dips);
         
     }, [dips]);
@@ -32,19 +37,26 @@ export const MenuList = () => {
                         return <MenuListItem key={ item.id } product={ item } />
                     })
                 }
-                <li className="menu__list-item">
-                    <div className="menu__list-item-upper">
-                        <p className="menu__list-item-name">{toUpper('dipsås')}</p>
-                        <span className="menu__dots"></span>
-                        <p className="menu__list-item-price">{`19 ${toUpper('sek')}`}</p>
-                    </div>
-                    <div className="menu__list-item-dips">
-                        {
-                            dips.map(dip => <span className="menu__list-item-dip">{dip.name}</span>)
-                        }
-                    </div>
-                </li>
+                {
+                    dips && <MenuSpecialListItem name={ 'dipsås' } price={ 19 } items={ dips } />
+                }
+                {
+                    drinks && <MenuSpecialListItem name={ 'dryck' } price={ 25 } items={ drinks } />
+                }
             </ul>
         </section>
     );
 }
+
+// <li className="menu__list-item">
+//                     <div className="menu__list-item-upper">
+//                         <p className="menu__list-item-name">{toUpper('dipsås')}</p>
+//                         <span className="menu__dots"></span>
+//                         <p className="menu__list-item-price">{`19 ${toUpper('sek')}`}</p>
+//                     </div>
+//                     <div className="menu__list-item-dips">
+//                         {
+//                             dips.map(dip => <span key={ dip.id } className="menu__list-item-dip">{dip.name}</span>)
+//                         }
+//                     </div>
+//                 </li>
