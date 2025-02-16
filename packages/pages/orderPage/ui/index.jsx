865fed18ce1aa3728ca1ab48f7bf1b20e9bb8cf3
Jesper Nyberg <91jesnyb@gafe.molndal.se>
2025-02-16 23:27:37 +0100

    last commits today

--- packages/pages/orderPage/ui/index.jsx
+++ packages/pages/orderPage/ui/index.jsx
@@ -12,15 +12,19 @@ export const OrderPage = () => {
     const navigate = useNavigate();
     
     useEffect(() => {
-        console.log(data?.order.items[0].name);
+        console.log(data?.order);
     }, [data]);
 
-    const calculateEta = (order) => {        
-        const timestampDate = new Date(order.timestamp);
-        const etaDate = new Date(order.eta);
-        const differenceMs = etaDate - timestampDate;
-        
-        return Math.floor(differenceMs / (1000 * 60));        
+    const calculateEta = () => {   
+        console.log()
+        if(!isLoading) {
+            const timestampDate = new Date(data?.order.timestamp);
+            const etaDate = new Date(data?.order.eta);
+            const differenceMs = etaDate - timestampDate;
+            return Math.floor(differenceMs / (1000 * 60));        
+        } else {
+            return 15;
+        }
     }
 
     return (
@@ -29,7 +33,7 @@ export const OrderPage = () => {
             <section className="order-details">
                 <img src="./src/assets/boxtop.png" alt="Take Away Box" className="order-details__image" />
                 <h1 className="order-details__msg">{toUpper('dina wontons tillagas!')}</h1>
-                <p className="order-details__eta">{toUpper(`eta ${calculateEta(data?.order)} min`)}</p>
+                <p className="order-details__eta">{toUpper(`eta ${calculateEta()} min`)}</p>
                 <p className="order-details__id">{toUpper(`#${data?.order.id}`)}</p>
             </section>
             <section className="order-page__btn-group">
