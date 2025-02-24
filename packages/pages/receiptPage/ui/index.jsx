45938f03506e4abc300afcf71ab08d34a0cdcdd9
Jesper Nyberg <91jesnyb@gafe.molndal.se>
2025-02-24 22:59:48 +0100

    basicpage setup done

--- packages/pages/receiptPage/ui/index.jsx
+++ packages/pages/receiptPage/ui/index.jsx
@@ -1,7 +1,26 @@
 import './index.css';
+import { useGetReceiptQuery } from '@yumyumApp/api';
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Header } from '@yumyumApp/header';
+import { Button } from '@yumyumApp/button';
 
 export const ReceiptPage = () => {
+    const [receipt, setReceipt] = useState(null);
+    const { id } = useParams();
+    const { data, isError, isLoading } = useGetReceiptQuery(id, { skip: !id });
+
+useEffect(() => {
+    if(!isLoading) {
+        setReceipt(data.receipt);
+        
+    }
+}, [data]);
+
     return (
-        <main>Receipt Page</main>
+        <main className="page receipt-page">
+                    <Header logo={ true } cartClass={'none'} />
+                    <Button text={ 'gör en ny beställning' } onClick={ () => navigate('/') } type={ 'solid' } />
+                </main>
     );
 }
